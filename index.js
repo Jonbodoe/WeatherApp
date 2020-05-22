ac2375296ee2888fa94b5a0642cbff2fccca15af
Jonbodoe <le0220@mail.philau.edu>
2020-05-21 22:00:37 -0400

    deploy & restructure files

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ if ( process.env.NODE_ENV === 'production') {
     app.use(express.static('../Frontend/build'))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../', 'Frontend', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'Frontend', 'build', 'index.html'));
     })
 }
 
