9f00b45ed535015b97962493a9c2c36a7f49877f
Jonbodoe <le0220@mail.philau.edu>
2020-05-24 00:33:17 -0400

    edit call handler

--- Frontend/src/Components/Navigation.js
+++ Frontend/src/Components/Navigation.js
@@ -19,7 +19,7 @@ const Navigation = () => {
         } else {
             // console.log(input.state)            
             axios
-                .post("http://localhost:5000/" || process.env.PUBLIC_URL, { query: queryList })
+                .post(process.env.PUBLIC_URL, { query: queryList })
                 .then(function (response) {
                     console.log(response)
                     if (!response.data.location) {
